a990f36a016395b3ec8c9096912d479264d9f5ef
PranavSheth12 <79102762+PranavSheth12@users.noreply.github.com>
2021-09-03 01:04:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Availability-of-vaccine-slots
+This is a program to find vaccine slots availability in any area using pincode.
+
+Steps: 1.] Run the program through the terminal.
+       2.] Enter the pincode and your mobile number.
+
+OH YES, Its that easy to find the slots near your area. You will be notified through sms about the slot availability.
---
+++ cowin_api.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import datetime
+import csv
+import os
+from twilio.rest import Client
+
+today = datetime.date.today().strftime("%d-%m-%Y")
+
+csv_file = open('cowin_slots.csv','a+',newline="")
+csv_writer = csv.writer(csv_file)
+# csv_writer.writerow(['Name','pincode','Vaccine','Availability'])
+
+pincode = input ("Enter your pincode : ")
+number = input("Enter your mobile number : ")
+
+r = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=' + pincode  +'&date='+today)
+CoData = json.loads(r.text)
+centers = CoData['centers']
+
+for i in centers:
+    # print(i)
+    name = i['name']
+    print(name)
+    s = i['sessions'][0]
+    # print(s)
+    capacity = s['available_capacity']
+    vaccine = s['vaccine']
+    print(f"Vaccine : {vaccine}")
+    print(f"Available slots : {capacity} ")
+    print(" ")
+
+    csv_writer.writerow([name,pincode,vaccine,capacity])
+    
+    if capacity > 0:
+        account_sid = 'Enter your api id here'
+        auth_token = 'Enter your auth token here'
+        client = Client(account_sid, auth_token)
+
+        message = client.messages \
+                        .create(
+                            body=f"Hey, {capacity} {vaccine} vaccine slots are availabe at {name}.",
+                            from_='+12242796914',
+                            to=f'{number}'
+                        )
+
+
+csv_file.close()
